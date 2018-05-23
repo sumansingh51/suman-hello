6386e8a273c89f8168f76c556970df7826ca99ed
sumansingh51 <suman.singh@dotsquares.com>
2018-05-23 17:51:10 +0530

    test

---
+++ Controller/Hello/index.php
@@ -0,0 +1,21 @@
+<?php
+namespace Suman\Hello\Controller\Hello;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\App\Action\Context;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    protected $pageFactory;
+    public function __construct(Context $context, PageFactory $pageFactory)
+    {
+        $this->pageFactory = $pageFactory;
+        return parent::__construct($context);
+    }
+
+    public function execute()
+    {        
+        var_dump(__METHOD__);
+        $page_object = $this->pageFactory->create();;
+        return $page_object;
+    }    
+}
